f4501a4e36afff93a1e69dd3795a6c097f40d3f4
Will Jordan <will.jordan@gmail.com>
2025-07-10 00:54:42 -0700

    add custom capabilities setting to registry config Allows a registry mirror to be configured with the "push" capability which can be helpful in some proxy use-cases.

--- util/resolver/resolver_test.go
+++ util/resolver/resolver_test.go
@@ -5,6 +5,7 @@ import (
 	"path"
 	"testing"
 
+	"github.com/containerd/containerd/v2/core/remotes/docker"
 	"github.com/moby/buildkit/cmd/buildkitd/config"
 	"github.com/stretchr/testify/require"
 )
@@ -65,4 +66,68 @@ mirrors = ["https://url/", "https://url/path/"]
 			require.Equal(t, h.Path, test.path)
 		}
 	}
+}
+
+func TestNewRegistryConfig(t *testing.T) {
+	const testConfig = `
+[registry."docker.io"]
+mirrors = ["yourmirror.local", "proxy.local:5000/proxy.docker.io"]
+
+[registry."yourmirror.local"]
+http = true
+
+[registry."proxy.local:5000"]
+capabilities = ["pull", "resolve", "push"]
+`
+
+	tests := map[string]struct {
+		description  string
+		http         bool
+		capabilities docker.HostCapabilities
+		mirrors      []string
+		scheme       string
+		path         string
+	}{
+		"registry-1.docker.io": {
+			description:  "valid_docker_io_with_mirrors",
+			mirrors:      []string{"yourmirror.local", "proxy.local:5000", "registry-1.docker.io"},
+			scheme:       "https",
+			path:         defaultPath,
+			capabilities: docker.HostCapabilityPull | docker.HostCapabilityResolve | docker.HostCapabilityPush,
+		},
+		"yourmirror.local": {
+			description:  "http_mirror",
+			scheme:       "http",
+			path:         defaultPath,
+			capabilities: docker.HostCapabilityPull | docker.HostCapabilityResolve,
+		},
+		"proxy.local:5000": {
+			description:  "proxy_push_mirror",
+			scheme:       "https",
+			path:         path.Join(defaultPath, "proxy.docker.io"),
+			capabilities: docker.HostCapabilityPull | docker.HostCapabilityResolve | docker.HostCapabilityPush,
+		},
+	}
+
+	cfg, err := config.Load(bytes.NewBuffer([]byte(testConfig)))
+	require.NoError(t, err)
+
+	require.NotEqual(t, 0, len(cfg.Registries))
+	registryHosts := NewRegistryConfig(cfg.Registries)
+	require.NotNil(t, registryHosts)
+	hosts, err := registryHosts("docker.io")
+	require.NoError(t, err)
+	hostnames := make([]string, 0, len(hosts))
+	for _, host := range hosts {
+		test := tests[host.Host]
+		desc := test.description
+		t.Run(desc, func(t *testing.T) {
+			hostnames = append(hostnames, host.Host)
+			require.NotNil(t, test, "unexpected registry host: %s", host.Host)
+			require.Equal(t, test.capabilities, host.Capabilities)
+			require.Equal(t, test.scheme, host.Scheme)
+			require.Equal(t, test.path, host.Path)
+		})
+	}
+	require.Equal(t, tests["registry-1.docker.io"].mirrors, hostnames)
 }
